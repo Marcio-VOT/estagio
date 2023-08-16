7f792e8d74d442b4d5cba6d5a9e1b68023407c87
Marcio-VOT <marciofsantos65@gmail.com>
2023-08-16 20:21:39 -0300

    feat: User Display Parcial;

--- src/components/GitHubUserSearch/GitHubUserSearch.tsx
+++ src/components/GitHubUserSearch/GitHubUserSearch.tsx
@@ -2,25 +2,32 @@
 import { User } from "@/protocols"
 import axios from "axios"
 import { useState } from "react"
+import UserDisplay from "../UserDisplay/UserDisplay";
 
-
-
-export default function GitHubUserSearch(){
+export default function GitHubUserSearch() {
   const [user, setUser] = useState<undefined | User>(undefined)
-  async function getUserData(data:FormData) {
-    const username = data.get('username') ?? ''
+  const [userName, setUserName] = useState<string>('')
+
+  async function getUserData(data: FormData) {
+    const username = data.get('username') ?? '';
     try {
       const { data: responseUser } = await axios.get<User>(`https://api.github.com/users/${username}`)
-      console.log("tudo ok ", responseUser.repos_url )
-    } catch (error : any) {
+      console.log("tudo ok ", responseUser)
+      setUser(responseUser)
+      setUserName('')
+    } catch (error: any) {
+      setUser(undefined)
       console.log("tudo não ok ", error.message)
     }
   }
 
   return (
-    <form action={getUserData}>
-      <input type="text" name="username" />
-      <button className="bg-gray-600">search</button>
-    </form>
+    <>
+      <form action={getUserData} className="w-full flex">
+        <input type="text" name="username" className="w-full rounded-s-lg outline-none pl-3 text-zinc-800" placeholder="GitHub Username..." value={userName} onChange={({ target: { value } }) => setUserName(value)} />
+        <button className="bg-zinc-700 hover:bg-zinc-800 border-l-2 p-1 rounded-e-lg border-gray-800">search</button>
+      </form>
+      {user && <UserDisplay user={user} />}
+    </>
   )
 }
